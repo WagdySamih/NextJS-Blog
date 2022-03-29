417afd4d259d326acec809baab67aa957b323c53
Wagdy Samih <wagdysamih787>
2022-03-29 03:58:52 +0200

    Added code highlight and custom image rendering to markdown

--- components/postsDetails/PostContent.js
+++ components/postsDetails/PostContent.js
@@ -1,13 +1,44 @@
+import Image from "next/image"
 import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { atomDark, a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+
 import PostHeader from "./PostHeader"
 
+import { constructImagePath } from "../../utils/helpers"
 import classes from "./PostContent.module.css"
 const PostContent = ({ post }) => {
+
+  const customRenderers = {
+
+    p({ node, children }) {
+      if (children[0].type == "img") {
+        const img = node.children[0].properties
+        return (
+          <div className={classes.image}>
+            <Image src={constructImagePath(img.src)} alt={img.alt} width={600} height={300} />
+          </div>
+        )
+      } else return <p>{children}</p>
+    },
+
+    code({ className, children }) {
+      // extract language from className 
+      const match = /language-(\w+)/.exec(className || '')
+      const language = match[1]
+      return (
+        <SyntaxHighlighter language={language} style={atomDark}>
+          {children}
+        </SyntaxHighlighter>
+      )
+    }
+
+  }
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={post.image} />
-      <ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>
         {post.content}
       </ReactMarkdown>
     </article>
