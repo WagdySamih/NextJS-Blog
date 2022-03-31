4a41927aab7e82816053ea5b44093d92891a626f
Wagdy Samih <wagdysamih787>
2022-03-31 17:12:11 +0200

    Added database connection and storage

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,17 +1,28 @@
-const handler = (req, res) => {
+import { connect, close } from "../../utils/database";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
-    const { email, name, message } = req.body;
-    if (!_isEmailValid(email)) return res.status(422).send({ message: "email is not valid" })
-    if (!_isMessageValid(message)) return res.status(422).send({ message: "message is not valid" })
-    if (!_isNameValid(name)) return res.status(422).send({ message: "name is not valid" })
-
-    const newMessage = {
-      name,
-      email,
-      message
+    try {
+      const { email, name, message } = req.body;
+      if (!_isEmailValid(email)) return res.status(422).send({ message: "email is not valid" })
+      if (!_isMessageValid(message)) return res.status(422).send({ message: "message is not valid" })
+      if (!_isNameValid(name)) return res.status(422).send({ message: "name is not valid" })
+  
+      const newMessage = {
+        name,
+        email,
+        message
+      }
+      const client = await connect();
+      const db = client.db();
+      const messages = await db.collection("messages")
+        .insertOne({ newMessage })
+      await close(client)
+  
+      res.status(201).send({ message: "message is sent successfully", messages })
+    } catch(error){
+      res.status(500).send(error)
     }
-
-    res.status(201).send({ message: "message is sent successfully" })
   }
 }
 
